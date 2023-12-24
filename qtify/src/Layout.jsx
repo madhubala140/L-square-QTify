fd85e46890417d5753b6de8fa5cfac8bf2d90e06
madhubala140 <madhusekar.s1994@gmail.com>
2023-12-24 11:03:45 +0530

    Add my new project

---
+++ qtify/src/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./components/NavBar/NavBar";
+import "./Layout.css";
+
+const Layout = () => {
+    return (
+      <div className="layout">
+        <NavBar />
+        </div>
+        );
+        }
+
+export default Layout;
