7d0ce5e5d4e0af3cecdf154ea7089c135074537e
madhubala140 <madhusekar.s1994@gmail.com>
2023-12-25 13:25:43 +0530

    hero changes

--- qtify/src/Layout.jsx
+++ qtify/src/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
+import HeroSection from "./components/HeroSection/HeroSection";
 import "./Layout.css";
 
 const Layout = () => {
     return (
       <div className="layout">
         <NavBar />
+        <HeroSection />
         </div>
         );
         }
